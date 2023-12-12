12448bffe5d97c456b9cd7f3df95cf0414561cae
agonzolm@myuax.com <agonzolm@myuax.com>
2023-12-12 12:18:45 +0100

    .

--- 2.cpp
+++ 2.cpp
@@ -9,6 +9,18 @@ union Dato {
 };
 
 int main() {
-    
+
     union Dato dato;
 
+   
+    dato.entero = 10;
+    printf("Valor entero: %d\n", dato.entero);
+
+    dato.flotante = 3.14;
+    printf("Valor flotante: %.2f\n", dato.flotante);
+
+    strcpy(dato.cadena, "Hola, mundo!");
+    printf("Valor cadena: %s\n", dato.cadena);
+
+    return 0;
+}
